a9c84d8fb5d9f68393a7e76bdefc1cdbbfbe93fe
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-22 12:22:26 +0100

    segundo commit

--- PrimoScanner.java
+++ PrimoScanner.java
@@ -9,12 +9,12 @@ public class PrimoScanner {
         Scanner sc =  new Scanner (System.in);
 
         System.out.print("¿Que numero quieres saber si es primo o no?: ");
-        int n = sc.nextInt();
+        int num = sc.nextInt();
 
         if (esPrimo(n)) {
-            System.out.print ("El numero " + n + " es primo");
+            System.out.print ("El numero " + num + " es primo");
         } else {
-            System.out.print ("El numero " + n + " no es primo");
+            System.out.print ("El numero " + num + " no es primo");
 
         }
 
